26de4c0c3f318ef182a640edc35c6311021f65a4
kkthecompguy <hnkhosamomollo3@gmail.com>
2021-11-14 16:02:39 +0300

    app bar config

--- app/src/main/java/com/kosamtech/triviaapp/MainActivity.kt
+++ app/src/main/java/com/kosamtech/triviaapp/MainActivity.kt
@@ -7,12 +7,13 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.kosamtech.triviaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.mainNavHostFragment)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
 
         // prevent nav gesture if not on start destination
